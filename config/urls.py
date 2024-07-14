96b90b7477ef41ab0f3535eaff1ca907f657b031
hyowon3434 <hyowon3434@naver.com>
2024-07-14 21:15:25 +0900

    Feat: 2주차 실습

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from pybo import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -9,3 +10,16 @@ def index(request):
     context = {'question_list':question_list}
 
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    print(f'question_id = {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set(content=request.POST.get('content'),
+                        create_date=timezone.now())
+
+    return redirect('pybo:detail', question_id=question.id)
